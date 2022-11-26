24a93aca229b4a61dfd7c93b7638702af2155683
Lie <1318211103@qq.com>
2022-11-26 22:02:56 +0800

    封装完善旋转动画，补充注释说明

--- ComposeAnimationKit/src/main/java/com/lie/composeanimationkit/view/LoadingAnimation.kt
+++ ComposeAnimationKit/src/main/java/com/lie/composeanimationkit/view/LoadingAnimation.kt
@@ -7,16 +7,27 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 
+/**
+ * 简单好用的旋转动画，传入需要旋转的子Composable作为内容
+ *
+ * @param durationMillis 动画旋转一周所用的时间，时间越长，旋转速度越慢
+ * @param delayTime 动画旋转一周后停留的时间
+ * @param easing 动画效果，默认线性：LinearEasing
+ * @param content 旋转的内容
+ */
 @Composable
 fun RotationAnimation(
-    time: Int = 3000,
+    durationMillis: Int = 3000,
     delayTime: Int = 0,
+    easing: Easing = LinearEasing,
     content: @Composable () -> Unit
 ) {
     val tran = rememberInfiniteTransition()
-    val rote by tran.animateFloat(initialValue = 0f, targetValue = 360f, animationSpec = infiniteRepeatable(
-        tween(time, delayTime, LinearEasing)
-    ) )
+    val rote by tran.animateFloat(
+        initialValue = 0f, targetValue = 360f, animationSpec = infiniteRepeatable(
+            tween(durationMillis, delayTime, easing)
+        )
+    )
     Box(
         Modifier.rotate(rote)
     ) {
