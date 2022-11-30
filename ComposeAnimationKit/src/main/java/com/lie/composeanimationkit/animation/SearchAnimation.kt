d596bb607e9ba13e5164fc2912306a083e57cd2c
Lie <1318211103@qq.com>
2022-11-30 19:09:25 +0800

    创建RadarSearchAnimationPreview

--- ComposeAnimationKit/src/main/java/com/lie/composeanimationkit/animation/SearchAnimation.kt
+++ ComposeAnimationKit/src/main/java/com/lie/composeanimationkit/animation/SearchAnimation.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.lie.composeanimationkit.AnimationKit
 import kotlin.math.min
@@ -83,4 +84,10 @@ fun SA.RadarSearchAnimation(
         }
     }
 
+}
+
+@Preview
+@Composable
+fun RadarSearchAnimationPreview() {
+    SA.RadarSearchAnimation()
 }
