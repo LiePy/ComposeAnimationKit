43f0cdcbc84ac0358809f4a2ba040b3a583c4b88
lei <lei.wang@mail.fxbusiness.cn>
2023-06-28 17:47:30 +0800

    1. Service实现生命周期控制，解决Activity退出后计时不显示问题

--- app/src/main/java/com/lie/composeanimationkit/FloatService.kt
+++ app/src/main/java/com/lie/composeanimationkit/FloatService.kt
@@ -26,13 +26,22 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
 import androidx.lifecycle.ViewTreeLifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import androidx.savedstate.SavedStateRegistry
+import androidx.savedstate.SavedStateRegistryController
+import androidx.savedstate.SavedStateRegistryOwner
 import androidx.savedstate.setViewTreeSavedStateRegistryOwner
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
-class FloatService : Service() {
+class FloatService : Service(), LifecycleOwner, SavedStateRegistryOwner {
     private lateinit var windowManager: WindowManager
+    private lateinit var lifecycleRegistry: LifecycleRegistry
+    private lateinit var savedStateRegistryController: SavedStateRegistryController
     private val layoutParams: WindowManager.LayoutParams by lazy {
         buildLayoutParams()
     }
@@ -41,18 +50,34 @@ class FloatService : Service() {
         buildComposeView()
     }
 
+    override fun onCreate() {
+        super.onCreate()
+        // 以下顺序很重要，控制自身Service生命周期
+        lifecycleRegistry = LifecycleRegistry(this)
+        savedStateRegistryController = SavedStateRegistryController.create(this)
+        savedStateRegistryController.performAttach()    // 此行可有可无，我们不调用performRestore内会帮我们调用
+        savedStateRegistryController.performRestore(null)
+        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    }
+
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         // 要用activity的 windowManager，否则token为null
+        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_START)
+        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
+
         windowManager = MainActivity.instance.windowManager
+
         openWindow()
         return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onDestroy() {
+        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+
         super.onDestroy()
         closeWindow()
     }
@@ -72,13 +97,13 @@ class FloatService : Service() {
     private fun buildComposeView(): View {
         val view = LayoutInflater.from(this).inflate(R.layout.view_win, null).apply {
             // set ViewTreeLifecycleOwner
-            ViewTreeLifecycleOwner.set(this@apply, MainActivity.instance)
+            ViewTreeLifecycleOwner.set(this@apply, this@FloatService)
         }
         view.findViewById<ComposeView>(R.id.tv_compose).apply {
             // set ViewTreeSavedStateRegistryOwner
-            setViewTreeSavedStateRegistryOwner(MainActivity.instance)
+            setViewTreeSavedStateRegistryOwner(this@FloatService)
             // set ViewTreeLifecycleOwner
-            ViewTreeLifecycleOwner.set(this@apply, MainActivity.instance)
+            ViewTreeLifecycleOwner.set(this@apply, this@FloatService)
 
             setContent {
                 FloatView()
@@ -94,14 +119,22 @@ class FloatService : Service() {
     private fun closeWindow() {
         windowManager.removeView(view)
     }
+
+    override val savedStateRegistry: SavedStateRegistry
+        get() = savedStateRegistryController.savedStateRegistry
+
+    override fun getLifecycle(): Lifecycle {
+        return lifecycleRegistry
+    }
 }
 
 @Composable
 fun FloatView() {
     var countDown by remember { mutableStateOf(0) }
 
+    val lifecycleScope = LocalLifecycleOwner.current.lifecycleScope
     LaunchedEffect(key1 = Unit, block = {
-        launch {
+        lifecycleScope.launchWhenResumed {
             while (true) {
                 delay(1000)
                 countDown++
